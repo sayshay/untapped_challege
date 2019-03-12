e4e25a52ebd8824eb08b4382b48d82a112125c44
Jordan Godwin <pelagic.dev@gmail.com>
2019-03-12 13:56:21 -0400

    Route to root URL and display data;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import axios from "axios";
+
+const API = "http://localhost:5000";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      conference: null,
+      teams: []
+    };
+  }
+
+  fetchData() {
+    axios.get(API, { mode: "no-cors" }).then(response => {
+      const data = response.data;
+      this.setState({ conference: data.conference, teams: data.teams });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    const { conference } = this.state;
+    const { teams } = this.state;
+
+    if (conference === null) {
+      return <h1>loading</h1>;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{conference.name}</h1>
+        {teams.map(team => (
+          <li key={team.id}>
+            {team.name} {team.mascot}
+          </li>
+        ))}
       </div>
     );
   }
