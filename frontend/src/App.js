8bd5ba5ef65320ccb18c18348873a7b1c7bd426e
Jordan Godwin <pelagic.dev@gmail.com>
2019-03-12 19:36:14 -0400

    Use ESLint & Prettier configs;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,55 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Box, Button, Card } from '@untappd/components'
 
-const API = "http://localhost:5000";
+const API = 'http://localhost:5000'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       conference: null,
-      teams: []
-    };
+      teams: [],
+    }
   }
 
   fetchData() {
-    axios.get(API, { mode: "no-cors" }).then(response => {
-      const data = response.data;
-      this.setState({ conference: data.conference, teams: data.teams });
-    });
+    axios.get(API, { mode: 'no-cors' }).then(response => {
+      const data = response.data
+      this.setState({ conference: data.conference, teams: data.teams })
+    })
   }
 
   componentDidMount() {
-    this.fetchData();
+    this.fetchData()
   }
 
   render() {
-    const { conference } = this.state;
-    const { teams } = this.state;
+    const { conference } = this.state
+    const { teams } = this.state
 
     if (conference === null) {
-      return <h1>loading</h1>;
+      return <h3>loading</h3>
     }
 
     return (
-      <div className="App">
-        <h1>{conference.name}</h1>
-        {teams.map(team => (
-          <li key={team.id}>
-            {team.name} {team.mascot}
-          </li>
-        ))}
-      </div>
-    );
+      <Box className="App" mx={8}>
+        <Card>
+          <Card.Header>{conference.name}</Card.Header>
+          <Card.Content>
+            <Button color="red">Testing</Button>
+
+            {teams.map(team => (
+              <li key={team.id}>
+                {team.name} {team.mascot}
+              </li>
+            ))}
+          </Card.Content>
+        </Card>
+      </Box>
+    )
   }
 }
 
-export default App;
+export default App
